584ece87ecd0acd5c951419ff1e2175b74f0d23d
emaniooo <eman.abubeker@yh.nackademin.se>
2026-08-23 19:13:23 +0200

    added Supabase login logic

---
+++ app.js
@@ -0,0 +1,29 @@
+const SUPABASE_URL = "https://ociqnhrzyyphecbdzjdw.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_0eSjt8UQ9jvlKiSBsU4EEw_m847MCqW";
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// LOGIN
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("login-form");
+  if (loginForm) {
+    loginForm.addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+
+      const { data, error } = await client.auth.signInWithPassword({
+        email,
+        password
+      });
+
+      if (error) {
+        document.getElementById("login-error").textContent = error.message;
+        return;
+      }
+
+      
+    });
+  }
+});
